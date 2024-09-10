f6cddfb0f21d267224e43b1da587fa7126644f6c
Jet Kingston <162489467+InsidiousVillian@users.noreply.github.com>
2024-09-10 13:36:39 +0200

    Add files via upload
    This fixes the problem with the hero and exitTile not spawning in random areas.

---
+++ Completed up to question 5.1/Fixed version question 2/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Fixed_version_question_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.Location = new System.Drawing.Point(120, 88);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(459, 228);
+            this.lblDisplay.TabIndex = 0;
+            this.lblDisplay.Text = "label1";
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(585, 256);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(75, 23);
+            this.btnUp.TabIndex = 1;
+            this.btnUp.Text = "btnUp";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(495, 308);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 2;
+            this.btnLeft.Text = "btnLeft";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(659, 308);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(84, 23);
+            this.btnRight.TabIndex = 3;
+            this.btnRight.Text = "btnRight";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(585, 363);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(75, 23);
+            this.btnDown.TabIndex = 4;
+            this.btnDown.Text = "btnDown";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.lblDisplay);
+            this.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnDown;
+    }
+}
+
---
+++ Completed up to question 5.1/Fixed version question 2/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Fixed_version_question_2
+{
+    public partial class Form1 : Form
+    {
+        private GameEngine engine;
+        public Form1()
+        {
+            InitializeComponent();
+            int gameLvls = 10;
+            //Initialize the instance varivble 
+            engine = new GameEngine(gameLvls);
+
+            //Call the method to hold the function to display
+            UpdateDisplay();
+
+            // Enable keyboard input
+            this.KeyPreview = true;
+
+            btnUp.Click += btnUp_Click;
+            btnDown.Click += btnDown_Click;
+            btnLeft.Click += btnLeft_Click;
+            btnRight.Click += btnRight_Click;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Initialize the variables that would be used for the tile of the game
+            int xValue = 12;
+            int yValue = 8;
+            //int positionParameter = 0;
+            //int xCoordinate = 12;
+            //int yCoordinate = 8;
+            int gameLvls = 10;
+
+            //Initialiaze the position object 
+            //Position position = new(xValue, yValue);
+
+            //Initialize the emptyTile object
+            //EmptyTile emptyTile;
+
+            //Initialize the object
+            //WallTile wallTile = new(positionParameter, xCoordinate, yCoordinate);
+
+            GameEngine gameEngine = new GameEngine(gameLvls);
+        }
+ 
+        public void UpdateDisplay()
+        {
+            //Set the label to a string message
+            lblDisplay.Text = engine.ToString();
+
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            MoveHero(Level.Direction.Up);
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            MoveHero(Level.Direction.Left);
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            MoveHero(Level.Direction.Down);
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            MoveHero(Level.Direction.Right);
+
+        }
+        private void MoveHero(Level.Direction direction)
+        {
+            engine.TriggerMovement(direction);
+            UpdateDisplay();
+        }
+    }
+
+
+}
+
+ 
